4f5b43941447508794f41152581c1109d0b5ec40
Stan <stanislas.girouard@gmail.com>
2025-03-18 16:04:12 +0100

    new rendu

--- src/components/Recherche/AddTaskForm.tsx
+++ src/components/Recherche/AddTaskForm.tsx
@@ -1,42 +1,46 @@
 import { useState } from "react"
 
-type TodoItem = {
-    id: number,
-    description: string
-    done: boolean
-}
+// type TodoItem = {
+//     id: number,
+//     description: string
+//     done: boolean
+// }
+
 let nextid = 0
 
 export default function AddTaskForm(){
     const [list, setList] = useState([])
-    const [text, setText] = useState("")
+    const [description, setDescription] = useState("")
+    const sortedList = 
+        list.sort((a, b) =>
+            a.done - b.done
+        )
 
-    function handleDelete(index) {
-        const newList = [...list];
-        newList.splice(index, 1);
+    function handleDelete(item) {
+        const newList = sortedList.filter(task => task.id !== item.id);
         setList(newList);
     };
 
     function editTask(id, completedValue) {
         setList(
-            list.map((task) =>
+            sortedList.map((task) =>
                 task.id === id ? { ...task, done: completedValue } : task
             )
         );
     };
-  
+
     return (
         <>
             <div className="flex">
                 <label className="input input-bordered flex items-center gap-2">
-                    <input type="text" value={text} onChange={e => setText(e.target.value)} className="grow" placeholder="Ajouter une tâche" />
+                    <input type="text" value={description} onChange={e => setDescription(e.target.value)} className="grow" placeholder="Ajouter une tâche" />
                 </label>
 
-                <button onClick={() => {setList([...list, {id: nextid++, text: text, done: false}]), setText("")}} className="btn btn-primary"> + </button>         
+                <button onClick={() => {setList([{id: nextid++, description: description, done: false}, ...list]), setDescription("")}} className="btn btn-primary"> + </button>         
             </div>
 
             <div className="my-5 flex-column gap-5 w-full text-left">
-                {list.map((task, index) => (
+                {list.map((task) => (
                     <div className={task.done ? "bg-indigo-900 w-full m-5 rounded-box p-3 flex" : "bg-indigo-700 w-full m-5 rounded-box p-3 flex"}>
                         <span className="pr-8">
                             <label>
@@ -44,10 +48,10 @@ export default function AddTaskForm(){
                             </label>
                         </span>
 
-                        <span key={task.id} className={task.done ? "flex-grow line-through" : "flex-grow"}> {task.text} </span>
+                        <span key={task.id} className={task.done ? "flex-grow line-through" : "flex-grow"}> {task.description} </span>
                         
                         <div>
-                            <button onClick={() => handleDelete(index)} className="btn btn-error btn-outline btn-xs">
+                            <button onClick={() => handleDelete(task)} className="btn btn-error btn-outline btn-xs">
                                 X
                             </button>
                         </div>
